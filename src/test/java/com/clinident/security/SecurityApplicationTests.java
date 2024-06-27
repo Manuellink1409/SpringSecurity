223aa7c29085e7ea7b2000aeda8c0e606d1eaf4c
Manuellink1409 <manuelaft14@gmail.com>
2024-06-26 22:56:43 -0600

    first commit

---
+++ src/test/java/com/clinident/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinident.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
